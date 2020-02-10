7c4cd18431d2302f7181c492a46d6e7bed46a1b6
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-02-11 01:55:46 +0530

    initial commit

---
+++ timer.js
@@ -0,0 +1,28 @@
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date('Feb 12, 2020 00:00:00').getTime(),
+  x = setInterval(function() {
+    let now = new Date().getTime(),
+      distance = countDown - now;
+
+    (document.getElementById('days').innerText = Math.floor(distance / day)),
+      (document.getElementById('hours').innerText = Math.floor(
+        (distance % day) / hour
+      )),
+      (document.getElementById('minutes').innerText = Math.floor(
+        (distance % hour) / minute
+      )),
+      (document.getElementById('seconds').innerText = Math.floor(
+        (distance % minute) / second
+      ));
+
+    if (distance < 0) {
+      document.getElementById('days').innerText = 0;
+      document.getElementById('hours').innerText = 0;
+      document.getElementById('minutes').innerText = 0;
+      document.getElementById('seconds').innerText = 0;
+    }
+  }, second);
