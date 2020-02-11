3639245f0124d6e229070429d8d66e7781d6d143
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-02-12 02:31:42 +0530

    update

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date('Feb 12, 2020 00:00:00').getTime(),
+let countDown = new Date('Feb 13, 2020 00:00:00').getTime(),
   x = setInterval(function() {
     let now = new Date().getTime(),
       distance = countDown - now;
@@ -26,3 +26,33 @@ let countDown = new Date('Feb 12, 2020 00:00:00').getTime(),
       document.getElementById('seconds').innerText = 0;
     }
   }, second);
+
+$(document).ready(function() {
+  $('.set > a').on('click', function() {
+    if ($(this).hasClass('active')) {
+      event.preventDefault();
+      $(this).removeClass('active');
+      $(this)
+        .siblings('.content')
+        .slideUp(200);
+      $('.set > a i')
+        .removeClass('fa-minus')
+        .addClass('fa-plus');
+    } else {
+      event.preventDefault();
+      $('.set > a i')
+        .removeClass('fa-minus')
+        .addClass('fa-plus');
+      $(this)
+        .find('i')
+        .removeClass('fa-plus')
+        .addClass('fa-minus');
+      $('.set > a').removeClass('active');
+      $(this).addClass('active');
+      $('.content').slideUp(200);
+      $(this)
+        .siblings('.content')
+        .slideDown(200);
+    }
+  });
+});
